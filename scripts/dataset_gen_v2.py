d9f42420816c5332329281750a340323d1597e63
Quatton <atomicatives@gmail.com>
2025-06-07 15:38:27 +0000

    eval

--- scripts/dataset_gen_v2.py
+++ scripts/dataset_gen_v2.py
@@ -1,24 +1,45 @@
 import json
 
 
-with open("data/lamp_reguessing_results.json") as f:
-    data = json.load(f)
-    results = data
+# with open("data/lamp_reguessing_results.json") as f:
+#     data = json.load(f)
+#     results = data
 
-with open("data/tokyo_lamps.json", "w") as f:
+# with open("data/tokyo_lamps.json", "w") as f:
+#     entries = []
+#     for result in results:
+#         user_msg = {
+#             "from": "human",
+#             "value": "Where is this place located in Tokyo? Provide observations of its key features, and based on that, reason about the ward it is located in. Your answer format should be an XML object with the following structure:  <observation>Details about the image without specifying the ward</observation><reasoning>Based on the observation, try to look for candidate wards that might match the image. If you are not sure, guess the ward that you think is most likely to match the image.</reasoning><ward>Ward name</ward> <image>",
+#         }
+#         assistant_msg = {"from": "gpt", "value": result["raw_content"]}
+#         conversations = [user_msg, assistant_msg]
+#         image = f"tokyo_2k/{result['id']}_{result['panoid']}.jpg"
+#         data = {
+#             "conversations": conversations,
+#             "image": image,
+#         }
+#         entries.append(data)
+
+#     json.dump(entries, f, indent=2, ensure_ascii=False)
+
+with open("../output_gpt.json") as f:
+    output = json.load(f)["results"]
+
+with open("data/has_lamp_v2.json") as f:
+    has_lamp_data = json.load(f)
+
+with open("data/tokyo_2k_v2_test.json", "w") as f:
     entries = []
-    for result in results:
-        user_msg = {
-            "from": "human",
-            "value": "Where is this place located in Tokyo? Provide observations of its key features, and based on that, reason about the ward it is located in. Your answer format should be an XML object with the following structure:  <observation>Details about the image without specifying the ward</observation><reasoning>Based on the observation, try to look for candidate wards that might match the image. If you are not sure, guess the ward that you think is most likely to match the image.</reasoning><ward>Ward name</ward> <image>",
-        }
-        assistant_msg = {"from": "gpt", "value": result["raw_content"]}
-        conversations = [user_msg, assistant_msg]
-        image = f"tokyo_2k/{result['id']}_{result['panoid']}.jpg"
-        data = {
-            "conversations": conversations,
-            "image": image,
-        }
-        entries.append(data)
+    output_with_lamp = [result for result in output if has_lamp_data[result["index"]]["has_lamp"] == "yes"]
 
+    for result in output_with_lamp:
+        entries.append(
+            {
+                "id": result["index"],
+                "panoid": result["panoid"],
+                "image": f"tokyo_2k_v2/{result['index']}_{result['panoid']}.jpg",
+                "ward": result["answer"]["ward"].strip().lower().replace("ō", "o").replace("ē", "e"),
+            }
+        )
     json.dump(entries, f, indent=2, ensure_ascii=False)
--- scripts/eval_gpt.py
+++ scripts/eval_gpt.py
@@ -12,8 +12,11 @@ api = OpenAI(api_key="0", base_url="http://0.0.0.0:8000/v1")
 def extract_info(text: str):
     """Extract ward."""
     ward_match = re.search(r"<ward>\s*(.*?)\s*</ward>", text)
+    ward = ward_match.group(1) if ward_match else None
+    if ward is None:
+        print(f"Warning: No ward found in text: {text}")
     return {
-        "ward": ward_match.group(1) if ward_match else None,
+        "ward": ward,
     }
 
 
@@ -25,28 +28,26 @@ with open("../output_gpt.json") as f:
 
 DATAROOT = "data/"
 
-FILE = "lora_3b_lamp"
+FILE = os.environ.get("FILE", "3b_lamp")
 
 
 def main():
-    existing_results = []
+    existing_results = {}
     # Load existing evaluation results
     try:
-        with open(f"../data/eval_results_{FILE}.json") as f:
-            existing_results = json.load(f)
-            evaluated_ids = {entry["id"] for entry in existing_results}
+        with open(f"data/eval_results_{FILE}.json") as f:
+            existing_results_list = json.load(f)
+            existing_results = {entry["id"]: entry for entry in existing_results_list}
 
     except FileNotFoundError:
-        evaluated_ids = set()
+        pass
 
-    entries = [entry for entry in dataset if int(entry["image"].split("/")[-1].split("_")[0]) not in evaluated_ids]
+    entries = [entry for entry in dataset if entry["id"] not in existing_results.keys()]
 
-    # Evaluate each entry
-    results = []
     for entry in entries:
-        image_path = entry["image"].replace("tokyo_2k/", "tokyo_2k_v2/")
+        image_path = entry["image"]
 
-        id = int(image_path.split("/")[-1].split("_")[0])
+        id = entry["id"]
 
         with open(os.path.join(DATAROOT, image_path), "rb") as img_file:
             image_data = img_file.read()
@@ -61,7 +62,11 @@ def main():
                     "content": [
                         {
                             "type": "text",
-                            "text": """Where is this place located in Tokyo? Provide observations of its key features, and based on that, reason about the ward it is located in. Your answer format should be an XML object with the following structure:  <observation>Details about the image without specifying the ward</observation><reasoning>Based on the observation, try to look for candidate wards that might match the image. If you are not sure, guess the ward that you think is most likely to match the image.</reasoning><ward>Ward name</ward>""",
+                            "text": """Where is this place located in Tokyo? Provide observations of its key features, and based on that, reason about the ward it is located in. Your answer format should be an XML object with the following structure:  <observation>Details about the image without specifying the ward</observation><reasoning>Based on the observation, try to look for candidate wards that might match the image. If you are not sure, guess the ward that you think is most likely to match the image.</reasoning><ward>Ward name only without any suffix</ward>
+
+                            List of wards in Tokyo:
+                            adachi, arakawa, bunkyo, chiyoda, chuo, edogawa, itabashi, katsushika, kita, koto, meguro, minato, nakano, nerima, oota, setagaya, shibuya, shinagawa, shinjuku, shibuya, suginami, sumida, taito.
+                            """,
                         },
                         {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image_base64}"}},
                     ],
@@ -69,8 +74,7 @@ def main():
             ],
         )
 
-        entry_info = extract_info(entry["conversations"][1]["value"])
-        expected_ward = entry_info.get("ward")
+        expected_ward = entry["ward"]
 
         result = {"id": id, "match": False, "expected": {"ward": expected_ward}}
         # Parse the response
@@ -82,7 +86,14 @@ def main():
         print(f"Evaluating ID: {id}")
 
         answer = extract_info(content)
+
         ward = answer.get("ward")
+
+        if ward is None:
+            print(f"Warning: No ward found in response for ID: {id}")
+            continue
+
+        ward = ward.lower().replace("ō", "o").replace("ē", "e")
 
         # Check if the answer matches
         print(f"Evaluating ID: {id}, Ward: {ward} vs Expected Ward: {expected_ward}")
@@ -94,15 +105,11 @@ def main():
             "raw": content,
         }
         result["match"] = match
-        results.append(result)
-
-        # Append new results to existing ones
-        if existing_results is not None:
-            results.extend(existing_results)
+        existing_results[id] = result
 
         # Save the updated evaluation results
         with open(f"data/eval_results_{FILE}.json", "w") as f:
-            json.dump(results, f, indent=2, ensure_ascii=False)
+            json.dump(sorted(existing_results.values(), key=lambda x: x["id"]), f, indent=2, ensure_ascii=False)
 
 
 if __name__ == "__main__":
